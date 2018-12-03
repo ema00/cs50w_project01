17997051ddf63dde093807bfe8d48ea71fa705dd
Emanuel Aguirre <ema_a_@hotmail.com>
2018-12-02 22:44:24 -0300

    first commit, starter templates

---
+++ application.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+"""
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+"""
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    # Get form information.
+    book_data = request.form.get("book")
+    # Redirect home, for now...
+    return redirect("/")
